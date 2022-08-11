c118fc8e51bd7ecc6c14802d7e23903a7601d774
THIAGO RIPARDO DE LIMA <tlima@sescmt.com.br>
2022-08-11 14:31:41 -0400

    refatorar funcao formatQuantity

--- libs/useFormatter.ts
+++ libs/useFormatter.ts
@@ -7,9 +7,12 @@ export const useFormatter = () => ({
         })
     },
     formatQuantity: (quantity: number, digits: number) => {
-        if(quantity < 10)
-            return `${'0'.repeat(digits)}${quantity}`;
 
-        return quantity;
+        if(quantity.toString().length >= digits)
+            return quantity;
+
+        const remain = digits - quantity.toString().length;
+        return `${'0'.repeat(remain)}${quantity}`;
+
     }
 });
