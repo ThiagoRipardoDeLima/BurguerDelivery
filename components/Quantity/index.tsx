cdb6aecd824f4d3e5b5f6fa7ff1e22e0d47dd6e2
THIAGO RIPARDO DE LIMA <tlima@sescmt.com.br>
2022-08-11 14:38:45 -0400

    Componente quantidade pt 4

--- components/Quantity/index.tsx
+++ components/Quantity/index.tsx
@@ -8,9 +8,10 @@ type Props = {
     onUpdateCount: (newCount: number) => void;
     min?: number;
     max?: number;
+    small?: boolean;
 }
 
-export const Quantity = ( { color, count, onUpdateCount, min, max }:Props ) => {
+export const Quantity = ( { color, count, onUpdateCount, min, max, small }:Props ) => {
     const [canRemove, setCanRemove] = useState(false);
     const [canAdd, setCanAdd] = useState(false);
 
@@ -38,16 +39,23 @@ export const Quantity = ( { color, count, onUpdateCount, min, max }:Props ) => {
                 onClick={handleRemove}
                 style={{
                     color: canRemove ? '#FFF' : '#96A3AB',
-                    backgroundColor: canRemove ? color : '#F2F4F5'
+                    backgroundColor: canRemove ? color : '#F2F4F5',
+                    width: small ? 42 : 48,
+                    height: small ? 42 : 48
                 }}
             >-</div>
-            <div className={ styles.qt }>{ formatter.formatQuantity(count, 1)}</div>
+            <div 
+                className={ styles.qt }
+                style={{ fontSize: small ? 16 : 18 }}
+            >{ formatter.formatQuantity(count, 1)}</div>
             <div 
                 className={ styles.button }
                 onClick={handleAdd}
                 style={{
                     color: canAdd ? '#FFF' : '#96A3AB',
-                    backgroundColor: canAdd ? color : '#F2F4F5'
+                    backgroundColor: canAdd ? color : '#F2F4F5',
+                    width: small ? 42 : 48,
+                    height: small ? 42 : 48
                 }}
             >+</div>
         </div>
